b173cd986f9346487dd396a388eb73f7fb4f92de
Jorlan <jorlanjr@gmail.com>
2019-11-24 01:49:52 -0300

    initial commit

---
+++ constants/articles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: 'Sorvete é feito de espuma...',
+    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    cta: 'Ver artigo', 
+    horizontal: true
+  },
+  {
+    title: 'Maquiagem é fake ne...',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Ver artigo'
+  },
+  {
+    title: 'Cabelos cacheados leg...',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Ver artigo' 
+  },
+  {
+    title: 'Roupa desnecessária para o frio de salv...',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'Ver artigo' 
+  },
+  {
+    title: 'Essa montanhas são lin…',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'Ver artigo', 
+    horizontal: true
+  },
+];
